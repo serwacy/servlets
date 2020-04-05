f7c617701489cb7df763e506822fc248655e5f55
Grzegorz <serwacy2@gmail.com>
2020-04-05 09:37:35 +0200

    removing yesterday (04.04) exercises

--- src/main/java/com/sda/QuizService.java
+++
@@ -1,7 +0,0 @@
-package com.sda;
-
-public class QuizService {
-    public boolean answerVerifier(final String submittedAnswer, final String correctAnswer){
-        return submittedAnswer.toLowerCase().equals(correctAnswer);
-    }
-}
